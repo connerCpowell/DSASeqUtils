d36a3e46014203502977ba996826cabad5fc2e79
malonge <malonge11@gmail.cm>
2016-10-11 16:21:53 -0700

    removed .idea directory

---
+++ dsa_seq_utils/utils/stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from __future__ import division
+import math
+
+
+def calculate_mean(data_list):
+    """
+    Calculate the mean of a list of numbers.
+    :param data_list: List of numbers
+    :return: Mean of those numbers.
+    """
+    if not data_list:
+        raise ValueError('At least 1 value is needed to calculate mean.')
+    return sum(data_list)/len(data_list)
+
+
+def calculate_pop_sd(data_list):
+    """
+    Calculate the population standard deviation of a list of numbers.
+    :param data_list: List of numbers.
+    :return: The population standard deviation of the list of numbers.
+    """
+    # Make sure there are enough data points.
+    sample_number = len(data_list)
+    if sample_number < 2:
+        raise ValueError('At least 2 data points needed to calculate population standard deviation.')
+
+    # Get the mean.
+    mean = calculate_mean(data_list)
+    # Calculate the variance.
+    variance = sum(math.pow(j - mean, 2) for j in data_list)/sample_number
+    return math.sqrt(variance)
